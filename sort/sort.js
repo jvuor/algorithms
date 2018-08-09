a8aede631a4a16fc79c381d05c48b5645d9c3c99
jvuor <jussi.vuorinen@gmail.com>
2018-08-09 15:17:50 +0300

    made a separate function for outputting results and prettified them

--- sort/sort.js
+++ sort/sort.js
@@ -7,10 +7,10 @@ var time = process.hrtime()
 
 const countTime = () => {
   // counts time from the last time the function was called
-  // returns the result as a prettified string
+  // returns the result as a tuple of [seconds, decimals]
   result = process.hrtime(time)
   time = process.hrtime()
-  return prettyTime(result)
+  return result
 }
 
 const createArray = (len) => {
@@ -33,9 +33,48 @@ const runTest = (len) => {
   builtInSort(createArray(len))
   const builtInResult = countTime()
 
-  console.log('\n\nResults, ' + len + ' elements\n-----------------\nInsertion sort:\t\t' + insertionResult +
-  '\nMerge sort:\t\t'+ mergeResult +
-  '\nJS .sort function:\t' + builtInResult)
+  prettyPrint({ len, insertionResult, mergeResult, builtInResult })
+}
+
+const prettyPrint = (results) => {
+  //outputs the results into console
+  //
+  //the print is pretty, the code isn't
+
+  let minIndex = null
+  let minResult = [+Infinity, +Infinity]
+  const resultArray = [results.insertionResult, results.mergeResult, results.builtInResult]
+
+  for (let i = 0; i < resultArray.length; i++) {
+    if (resultArray[i][0] < minResult[0] || (resultArray[i][0] === minResult[0] && resultArray[i][1] < minResult[1])) {
+      minResult = resultArray[i]
+      minIndex = i
+    }
+  }
+
+  if( minIndex === null ) { throw 'Error with printing results' }
+
+  process.stdout.write('\x1b[4m\x1b[36mResults for an array with ' + results.len +' elements\x1b[0m\n\n')
+  
+  process.stdout.write('Insertion sort\t\t')
+  minIndex === 0?
+  process.stdout.write('\x1b[32m' + prettyTime(results.insertionResult) + '\x1b[0m') :
+  process.stdout.write(prettyTime(results.insertionResult))
+  process.stdout.write('\n')
+
+  process.stdout.write('Merge sort\t\t')
+  minIndex === 1?
+  process.stdout.write('\x1b[32m' + prettyTime(results.mergeResult) + '\x1b[0m') :
+  process.stdout.write(prettyTime(results.mergeResult))
+  process.stdout.write('\n')
+
+  process.stdout.write('JS Array.sort()\t\t')
+  minIndex === 2?
+  process.stdout.write('\x1b[32m' + prettyTime(results.builtInResult) + '\x1b[0m') :
+  process.stdout.write(prettyTime(results.builtInResult))
+  process.stdout.write('\n')
+
+  console.log('\n')
 }
 
 module.exports = { countTime, createArray, runTest }
